5615a2fc36b3cf80e69396936a07825e4a9b0bd6
Diogoepronto <bta.diogo@gmail.com>
2023-06-19 16:23:56 +0100

    Init project

---
+++ ClinicaPetHeroWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClinicaPetHeroWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
